c0a8a70f257b818ebadda69cb9f06cc46628938c
Darren <donaldwu1101@gmail.com>
2017-08-11 19:57:09 +0800

    login function

--- TwitterPHPDemo/LoginViewController.swift
+++ TwitterPHPDemo/LoginViewController.swift
@@ -25,7 +25,57 @@ class LoginViewController: UIViewController {
             passwordText.attributedPlaceholder = NSAttributedString(string: "password", attributes: [NSAttributedStringKey.foregroundColor: UIColor.red])
         } else {
             
+            // Shortcuts
+            let username = usernameText.text!.lowercased()
+            let password = passwordText.text!
+            
             // Send request to mysql db
+            // Url to accesss out php file
+            let url = URL(string: "http://localhost/Twitter/login.php")!
+            
+            // Request url
+            var request = URLRequest(url: url)
+            
+            // Method to ppass data POST - cause it is secured
+            request.httpMethod = "POST"
+            
+            // Body gonna be appended to url
+            let body = "username=\(username)&password=\(password)"
+            request.httpBody = body.data(using: String.Encoding.utf8)
+            
+            // Launch session
+            URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
+                
+                if error == nil {
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as? NSDictionary
+                        guard let parseJSON = json else {
+                            print("Error")
+                            return
+                        }
+
+                        print(parseJSON)
+                        
+                        let id = parseJSON["id"] as? String
+                        
+                        if id != nil {
+                            
+                            // Successfully logged in
+                            // Move to next page
+                            
+                        }
+                        
+                    } catch {
+                        print("Caught an error: \(error)")
+                    }
+                    
+                } else {
+                    print("Error: \(String(describing: error))")
+                }
+                
+                
+            }).resume()
+            
             
         }
     }
